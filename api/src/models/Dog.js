cd83a6431939180c133190a36e0d99f55fca9534
GabrielDenis <83370739+GabrielDenis@users.noreply.github.com>
2022-06-20 18:46:08 -0300

    database seteada

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,38 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    lifeSpan: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+
+    createdInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
+
   });
 };
